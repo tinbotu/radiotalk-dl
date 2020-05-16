3e8e209ebb1d49fa8f90bc04fb698c08de3f27ea
Akira KUMAGAI <kuma@ultrasync.net>
2020-05-17 00:47:48 +0900

    trivial
    trivial
    
    Refactor
    
    Refactor

--- radiotalk-dl.py
+++ radiotalk-dl.py
@@ -19,11 +19,11 @@ class RadiotalkDl(object):
     _id_re = None
 
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._id_re = re.compile(r'\/talk\/([0-9]+)')
 
 
-    def get_program_id(self, original_url):
+    def get_program_id(self, original_url: str) -> int:
         m = self._id_re.search(original_url)
         if m and m.group():
             self.program_id = int(m.group(1))
@@ -32,33 +32,41 @@ class RadiotalkDl(object):
             raise ValueError("Can't extract programid")
 
 
-    def get_program(self, program_id):
+    def get_program(self, program_id: int) -> dict:
         url = (self._api_url % (program_id))
         r = requests.get(url)
         return json.loads(r.text)
 
 
-    def get_program_name(self, p, timestamp: str):
+    def get_program_name(self, p, timestamp: str) -> str:
         return p['programTitle'] + ' - ' + str(p['id']) + ' ' + p['title'] + ' (' + timestamp + ')'
 
 
-    def get_audio_url(self, p):
+    def get_audio_url(self, p: str) -> str:
         return p['audioFileUrl']
 
-    def download(self, url, filename, timestamp):
-        filename = self.outdir + "/%s.m4a" % (filename)
-        r = requests.get(url)
+
+    def write_as_file(self, filename: str, content: str, timestamp: float):
         with open(filename, "wb") as fp:
-            fp.write(r.content)
+            fp.write(content)
         os.utime(filename, (timestamp, timestamp))
 
-    def get_audio(self, url):
+
+    def download(self, url: str, program_name: str, timestamp: float):
+        filename = self.outdir + "/%s.m4a" % (program_name)
+        r = requests.get(url)
+        self.write_as_file(filename, r.content, timestamp)
+
+
+    def get_audio(self, url: str) -> str:
         program_id = self.get_program_id(url)
         program = self.get_program(program_id)
+
         tm = time.strptime(program['createdAt'], '%Y-%m-%d %H:%M:%S')
         program_timestamp = time.mktime(tm)
         program_timestamp_str = "%04d-%02d-%02d" % (tm.tm_year, tm.tm_mon, tm.tm_mday)
         program_name = self.get_program_name(program, program_timestamp_str)
+
         self.download(self.get_audio_url(program), program_name, program_timestamp)
 
         return program_name
